ee30b529036403b5878bbbfe4ffd68c425bf27eb
torres-developer <torres.dev@disroot.org>
2024-12-01 14:25:25 +0300

    basic impl

---
+++ README.md
@@ -0,0 +1,12 @@
+# saat
+
+O meu prof de Sistemas Operativos, que continua a usar o Windows Vista, abriu o
+xclock (ou um outro parecido) no meio da aula e disse:
+
+> Mano bro, isto deve ser complicado de fazer, o tempo muda em tempo real!
+
+Este gajo é burres só pode.
+
+Estes profs napcebem nada. Acabaram os estudes à vintanes e foram logo dar
+aulas. Passaram a sua vida toda a ler os mesmos powerpoints cada ano que passa.
+Desatualizades.
--- clock.c
+++ clock.c
@@ -1,10 +1,13 @@
 #include "./raylib.h"
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 #define INIT_SIDE 720
 #define TITLE "saat"
+
+void ending_pos(Vector2 *v, float angle, int center, float len);
 
 int main(void) {
 
@@ -12,9 +15,10 @@ int main(void) {
 
   SetTargetFPS(1);
 
+  printf("epoch: %d\n", (int)time(NULL));
   int now = (int)time(NULL) % (60 * 60 * 24);
 
-  int hour = now % (60 * 60 * 24);
+  int hour = now / (60 * 60);
   float hour_angle = hour * (PI / 6) - (PI / 2);
   int minute = (now % (60 * 60)) / 60;
   float minute_angle = minute * (PI / 30) - (PI / 2);
@@ -24,6 +28,8 @@ int main(void) {
   printf("epoch: %d\n%d:%d:%d\n", now, hour, minute, second);
 
   int center = INIT_SIDE / 2;
+  Vector2 centerV = {.x = center, .y = center};
+  Vector2 endV;
 
   while (!WindowShouldClose()) {
     hour_angle += 1.0 / (60 * 60 * 6);
@@ -33,16 +39,19 @@ int main(void) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawCircleLines(center, center, (float)center, BLACK);
-    DrawLine(center, center, center + cos(hour_angle) * ((float)center / 2),
-             center + sin(hour_angle) * ((float)center / 2), DARKGRAY);
-    DrawLine(center, center,
-             center + cos(minute_angle) * (((float)center * 2) / 3),
-             center + sin(minute_angle) * (((float)center * 2) / 3), DARKGRAY);
-    DrawLine(center, center,
-             center + cos(second_angle) * (((float)center * 3) / 4),
-             center + sin(second_angle) * (((float)center * 3) / 4), DARKGRAY);
+    ending_pos(&endV, hour_angle, center, 1.0 / 2);
+    DrawLineEx(centerV, endV, 2, DARKGRAY);
+    ending_pos(&endV, minute_angle, center, 2.0 / 3);
+    DrawLineEx(centerV, endV, 2, DARKGRAY);
+    ending_pos(&endV, second_angle, center, 3.0 / 4);
+    DrawLineV(centerV, endV, DARKGRAY);
     EndDrawing();
   }
 
   return 0;
+}
+
+void ending_pos(Vector2 *v, float angle, int center, float len) {
+  v->x = center + cos(angle) * (float)center * len;
+  v->y = center + sin(angle) * (float)center * len;
 }
